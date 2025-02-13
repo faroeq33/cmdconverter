2aa8e16b0e59b892638099a7fd241f0850b0e159
Faroeq <alves_1044@hotmail.com>
2025-02-13 21:35:45 +0100

    Move functioncalls to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { convertNpmToPnpm } from "./cmd-converter.ts";
+
+// Example usage
+
 function App() {
   return (
     <>
-      <div></div>
-      <h1>Vite + React</h1>
+      <h1>Convert npm command to pnpm command</h1>
+      <p>poop</p>
+      <div>{convertNpmToPnpm("install express")}</div>
+      <div>{convertNpmToPnpm("install --save-dev webpack")}</div>
+      <div>{convertNpmToPnpm("uninstall lodash")}</div>
+      <div>{convertNpmToPnpm("run build")}</div>
+      <div>{convertNpmToPnpm("cache clean")}</div>
+
       <div className="card"></div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-import { convertNpmToPnpm } from "./cmd-converter.ts";
-
-// Example usage
-console.log(convertNpmToPnpm("install express"));
-console.log(convertNpmToPnpm("install --save-dev webpack"));
-console.log(convertNpmToPnpm("uninstall lodash"));
-console.log(convertNpmToPnpm("run build"));
-console.log(convertNpmToPnpm("cache clean"));
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
